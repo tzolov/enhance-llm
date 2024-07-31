7ff99e1058b20fa848052d5c903dca84149affdb
Dave Syer <dsyer@vmware.com>
2024-07-31 10:55:38 +0100

    Copy some code and make it work

---
+++ shell.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> { };
+
+mkShell {
+
+  name = "env";
+  buildInputs = [
+    python3Packages.python
+    python3Packages.venvShellHook
+  ];
+
+  venvDir = "./.venv";
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+    pip install --prefix=.venv jupyter ipykernel
+    pip install --prefix=.venv transformers torch einops accelerate datasets chromadb sentence-transformers
+  '';
+
+  postShellHook = ''
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH";
+  '';
+
+}
